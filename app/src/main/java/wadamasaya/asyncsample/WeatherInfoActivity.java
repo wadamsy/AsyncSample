e89085cd59c175baac6abd51083a5ac8abca2100
wadamasaya <16010418@anabuki-college.ac.jp>
2018-07-19 09:34:37 +0900

    init

---
+++ app/src/main/java/wadamasaya/asyncsample/WeatherInfoActivity.java
@@ -0,0 +1,13 @@
+package wadamasaya.asyncsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_info);
+    }
+}
